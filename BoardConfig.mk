1786c00f2d7cc97ffc0b0a45e4890b3195385f25
1vivy <1vivy@tutanota.com>
2026-06-01 02:04:38 -0600

    infiniti: wire OplusCamera oemlayer.v2 + videodehaze; allow vendor sepolicy types
    - proprietary-files: add odm/lib64/hw/camera.oemlayer.v2.so and
      odm/lib64/camera/components/com.oplus.node.videodehaze.so (DISABLE_CHECKELF)
    - BoardConfig: BUILD_BROKEN_TREBLE_SEPOLICY_NEW_PUBLIC_SYMBOLS for unofficial build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,5 +35,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
+# Allow device/vendor-specific SELinux types without compat mapping entries (unofficial build)
+BUILD_BROKEN_TREBLE_SEPOLICY_NEW_PUBLIC_SYMBOLS := true
+
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/infiniti/BoardConfigVendor.mk
