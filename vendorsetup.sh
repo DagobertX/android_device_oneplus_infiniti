c32cbcfbac5de45853e1c5accd61d6917bfc6f08
dagobert <73406317+fuckdedeurkruk@users.noreply.github.com>
2026-06-05 10:52:51 +0200

    infiniti: Rename clang toolchain directory

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-mkdir -p "prebuilts/clang/host/linux-x86/neutron-clang"
-cd "prebuilts/clang/host/linux-x86/neutron-clang"
+mkdir -p "prebuilts/clang/host/linux-x86/clang-neutron"
+cd "prebuilts/clang/host/linux-x86/clang-neutron"
 curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"
 chmod +x antman
 ./antman -S
